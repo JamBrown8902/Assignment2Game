b94470da3511420481390b319236439c8bbce3de
James Shiba <jamiebrown.jobs@gmail.com>
2021-11-15 12:36:18 +0000

    Added comments for Sliding Platform class

--- classes/SlidingPlatform.js
+++ classes/SlidingPlatform.js
@@ -1,56 +1,70 @@
+//Defines Sliding Platform class
+//Sliding platforms are blue platforms that move from left to right, making it more difficult to jump to
 class SlidingPlatform {
 	constructor(posX,posY,width,height) {
+		//Created using positional parameters (posX, posY) and size parameters (width, height)
+
+		//Defines options to be used when creating Matter body
 		let options = {
 			isStatic: true,
 			restitution: 0,
 			friction: 0.5
 		}
+
+		//Creates new attributes using parameters
 		this.posX = posX;
 		this.posY = posY;
 		this.width = width;
 		this.height = height;
-		this.type = "sliding";
-		this.body = Matter.Bodies.rectangle(this.posX, this.posY, this.width, this.height, options);
-		this.movementRange = get_random(50,100);
-		this.movementSpeed = get_random(1,2);
-		this.originalX = posX
-		this.moveLeft = false;
+		this.originalX = posX; //Position for platform to move around
 
+		//Creates new attributes using default values
+		this.type = "sliding"; //Type of platform
+		this.movementRange = get_random(50,100); //Random length of movement
+		this.movementSpeed = get_random(1,2); //How quickly the platform moves
+		this.moveLeft = false; //If the platform is moving left
+
+		//Creates Matter body to handle collisions
+		this.body = Matter.Bodies.rectangle(this.posX, this.posY, this.width, this.height, options);
 		Matter.World.add(world, this.body);
 	}
 	draw() {
-		let pos = this.body.position; //create an shortcut alias
+		//Draw method updates position and shows the object
 
-		if(this.moveLeft == true) {
-			pos.x = pos.x + this.movementSpeed;
-			if(pos.x >= this.originalX + this.movementRange) {
-				this.moveLeft = false;
+		let pos = this.body.position; //Create an shortcut alias
+
+		if(this.moveLeft == false) { //If object is moving right..
+			pos.x = pos.x + this.movementSpeed; //Move platform to the right using movement speed
+			if(pos.x >= this.originalX + this.movementRange) { //If platform has reached maximum range...
+				this.moveLeft = true; //Change direction
 			}
-		} else {
-			pos.x = pos.x - this.movementSpeed;
-			if(pos.x <= this.originalX - this.movementRange) {
-				this.moveLeft = true;
+		} else { //If platform is moving left...
+			pos.x = pos.x - this.movementSpeed; //Move platform to the left using movement speed
+			if(pos.x <= this.originalX - this.movementRange) { //If platform has reached maximum range...
+				this.moveLeft = false; //Change direction
 			}
 		}
-		let newPos;
-		pos.y = pos.y + globalHeight;
-		if(this.moveLeft == true) {
-			newPos = Matter.Vector.create(this.movementSpeed,globalHeight);
+
+		let newPos; //Defines local variable to store new vector
+
+		pos.y = pos.y + globalHeight; //Updates height to imitate scrolling
+
+		if(this.moveLeft == true) { //Checks what direction platform is moving to create correct vector
+			newPos = Matter.Vector.create(this.movementSpeed,globalHeight); //Updates newPos Vector
 		} else {
-			newPos = Matter.Vector.create(-this.movementSpeed,globalHeight);
+			newPos = Matter.Vector.create(-this.movementSpeed,globalHeight); //Updates newPos Vector
 		}
 		
+		//Moves bounds to new position to ensure collisions remain accurate
 		Matter.Bounds.translate(this.body.bounds, newPos);
-		rectMode(CENTER); //switch centre to be centre rather than left, top
-		fill('#0000ff'); //set the fill colour
-		rect(pos.x, pos.y, this.width, this.height); //draw the rectangle
 
+		//Draws rectangle
+		rectMode(CENTER); //Switch centre to be centre rather than left, top
+		fill('#0000ff'); //Set the fill colour
+		rect(pos.x, pos.y, this.width, this.height); //Draw the rectangle
+
+		//Updates positional attributes
 		this.posX = pos.x;
 		this.posY = pos.y;
-		
-		
-		
-		
-		
 	}
 }
